eb18c9d3bec691d98a24e1cdc5292afe682d819a
Shailee <shailee.mehta@byteprophecy.com>
2017-10-13 20:16:47 +0530

    modified executeQuery

--- MSSQLExecutor.js
+++ MSSQLExecutor.js
@@ -16,7 +16,7 @@ function executeQuery(connection, rawQuery, cb) {
     } else {
       cb({
         status: true,
-        content: results
+        content: results.recordsets
       });
     }
   });
